687dc076628e418f31430819ec7acac5fdf0cf86
Sen <puja.sen@cgi.com>
2021-06-13 19:08:02 +0530

    Add project files.

---
+++ ImmutableObjects.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    //https://www.c-sharpcorner.com/article/all-about-c-sharp-immutable-classes2/
+    // Immutable Class means a class which once created, its state cannot be changed later by any means internally or externally.
+    //Read Only properties
+    // Parametrized constructor to initialize properties,
+    class ImmutableObjects
+    {
+        public FullName name { get; }
+        public ImmutableObjects(string _firstName, string _lastName)
+        {
+            this.name = new FullName();
+            this.name.firstName = _firstName;
+            this.name.lastName = _lastName;
+        }      
+    }
+
+    class FullName
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class Singleton
+    {
+    }
+
+    public sealed class SingletonPatternWithoutThreadSafety
+    {
+        private SingletonPatternWithoutThreadSafety() { }
+        private static SingletonPatternWithoutThreadSafety _instance = null;
+        
+        public static SingletonPatternWithoutThreadSafety Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new SingletonPatternWithoutThreadSafety();
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    public sealed class SingletonPatternDoubleLock
+    {
+        private SingletonPatternDoubleLock() { }
+        private static SingletonPatternDoubleLock _instance = null;
+        private static readonly object obj = new object();
+
+        public static SingletonPatternDoubleLock Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (obj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SingletonPatternDoubleLock();
+                        }
+                    }
+                }
+             
+                return _instance;
+            }
+        }
+    }
+
+    public sealed class SingletonPatternStatic
+    {
+        private SingletonPatternStatic() { }
+        static SingletonPatternStatic() { }
+        private static readonly SingletonPatternStatic _instance = new SingletonPatternStatic();
+
+        public static SingletonPatternStatic Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+
+    public sealed class SingletonPatternLazy
+    {
+        private SingletonPatternLazy() { }
+        private static readonly Lazy<SingletonPatternLazy> _instance = new Lazy<SingletonPatternLazy>(()=> new SingletonPatternLazy());
+
+        public static SingletonPatternLazy Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
